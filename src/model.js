5dc4245fec9ac14de9039ef8b33b035e562dcb40
Nicholas Young <nyoung@untilnow.co>
2016-02-29 18:43:09 -0600

    enhancement: support a mostly-functional API. Closes #1.

--- src/model.js
+++ src/model.js
@@ -17,7 +17,7 @@ export default class Model {
     return this._type;
   }
 
-  get _document() {
+  toJSON() {
     return {
       id: this._id,
       type: this._type,
@@ -33,45 +33,4 @@ export default class Model {
     return set(this._payload, key, value);
   }
 
-  save(db) {
-    const storage = db.sublevel(this.type);
-
-    return new Promise((resolve, reject) => {
-      storage.put(this.id, this._document, err => {
-        if (err) {
-          return reject(err);
-        }
-
-        resolve(this);
-      });
-    })
-  }
-
-  static find(db, id, type) {
-    const storage = db.sublevel(type);
-    
-    return new Promise((resolve, reject) => {
-      storage.get(id, (err, value) => {
-        if (err) {
-          return reject(err);
-        }
-
-        const {
-          id,
-          type,
-          ...attributes
-        } = value;
-
-        resolve(new Model(id, type, attributes));
-      });
-    });
-  }
-
-  static findOrCreate(db, id, type, payload = {}) {
-    return this.find(db, id, type).then(record => {
-      return record;
-    }).catch(() => {
-      return new this(id, type, payload).save(db);
-    });
-  }
 }
