3537a6efd3ad875c6900de33cdde68f2951b385f
Nicholas Young <nyoung@untilnow.co>
2016-03-10 23:42:09 -0600

    fix: rename `createClient` to `configureClient`.

--- test/clients/http.test.js
+++ test/clients/http.test.js
@@ -1,18 +1,18 @@
 import test from 'ava';
-import { createClient } from '../../src/clients/http';
+import { configureClient } from '../../src/clients/http';
 
-const client = createClient('http://localhost:4000');
+const client = configureClient('http://localhost:4000');
 
-test('createClient is a function', t => {
-  t.is(typeof createClient, 'function');
+test('configureClient is a function', t => {
+  t.is(typeof configureClient, 'function');
 });
 
-test('createClient() returns a function', t => {
-  t.is(typeof createClient('http://localhost:4000'), 'function');
+test('configureClient() returns a function', t => {
+  t.is(typeof configureClient('http://localhost:4000'), 'function');
 });
 
-test('createClient() accepts an initialization object', t => {
-  t.is(typeof createClient({
+test('configureClient() accepts an initialization object', t => {
+  t.is(typeof configureClient({
     base: 'http://localhost:4000'
   }), 'function');
 });
