5f12ff852deae2c88f5633235f5ac4ad112146fc
Nicholas Young <nyoung@untilnow.co>
2016-03-13 16:50:25 -0500

    enhancement: add tests for `sync` via http.

--- test/meridian.test.js
+++ test/meridian.test.js
@@ -2,6 +2,9 @@ import test from 'ava';
 import { setup } from '../src';
 import product from './fixtures/product';
 import products from './fixtures/products';
+import app from '../example/app';
+
+app.listen(4000);
 
 const client = setup({
   base: 'http://localhost:4000'
@@ -9,15 +12,54 @@ const client = setup({
 
 test('sync()', async t => {
   const result = await client.sync(products, { raw: true });
+
+  t.is(Object.keys(result).length, 2);
 });
 
 test('sync() with existing record handles refresh', async t => {
   const multi  = await client.sync(products, { raw: true });
   const single = await client.sync(product, { raw: true });
+
+  t.not(single.products[0].refreshed_at, undefined);
 });
 
-test('sync() http', async t => {
+test('sync() via http', async t => {
   const result = await client.sync({
     endpoint: '/products.json'
   });
+
+  const keys = Object.keys(result);
+  
+  t.is(typeof result, 'object');
+  t.not(keys.indexOf('products'), -1);
+  t.not(keys.indexOf('categories'), -1);
+});
+
+test('sync() without required params', async t => {
+  try {
+    const result = await client.sync({});
+  } catch (err) {
+    t.not(err, undefined);
+  }
+});
+
+test('sync() with bad endpoint', async t => {
+  try {
+    const result = await client.sync({
+      endpoint: 'xxx'
+    });
+  } catch (err) {
+    t.not(err, undefined);
+  }
+});
+
+test('sync() with unauthorized endpoint', async t => {
+  try {
+    const result = await client.sync({
+      endpoint: '/401'
+    });
+  } catch (err) {
+    t.not(err, undefined);
+    t.same(err, ['unauthorized req']);
+  }
 });
